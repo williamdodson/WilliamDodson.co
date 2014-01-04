680ac6c3d597393a2f8f7e6eadf50839fb9a4b42
William Dodson <william.dodson@gmail.com>
2014-01-04 17:21:54 -0500

    Initial commit.

---
+++ config.rb
@@ -0,0 +1,14 @@
+# Require any additional compass plugins here.
+
+http_path = "/"
+css_dir = "/a/c/"
+sass_dir = "sass"
+images_dir = "/a/i/"
+javascripts_dir = "/a/j/"
+
+output_style = :compressed
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+
+preferred_syntax = :scss
